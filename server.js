423be5050eadaee347e2e1cfb262ce47f1d06f11
desprest <despres_theo@hotmail.com>
2018-09-19 15:18:47 +0200

    fc

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+//var cfenv = require("cfenv");
+//var appEnv = cfenv.getAppEnv();
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.info("Listening on port: " + port);
+});
+
+app.get('/', function (req, res) {
+	res.type("text/html").status(200).send('<html><head></head><body><h1>Welcome to my first CF Node App !</h1><br /><a href="/Get">Do a Get Call</a></body></html>');
+});
+
+app.get('/Get', function (req, res) {
+	res.type("text/html").status(200).send('<html><head></head><body><h1>Get OK !</h1><br /><a href="/">Back</a></body></html>');
+});
